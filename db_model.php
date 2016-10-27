9120d5069e6ed2760068fb82f3692b316eea3ab7
stefkundig <stefkundig@yahoo.gr>
2016-10-27 15:02:56 +0200

    fixes

--- db_model.php
+++ db_model.php
@@ -2,8 +2,6 @@
 $servername = "localhost";
 $username = "root";
 $password = "uTh6ap8eesei";
-=======
-$password = "";
 
 $conn = mysqli_connect($servername, $username, $password);
 mysqli_set_charset($conn,"utf8");
@@ -12,4 +10,5 @@ if (!$conn)
 		die("Connection failed: ".mysqli_connect_error());
 }
 $sql = "USE model";
-mysqli_query($conn, $sql);?> 
+mysqli_query($conn, $sql);
+?> 
--- insertValueCrowd.php
+++ insertValueCrowd.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include 'db_dataset.php';
 include 'json.php';
@@ -48,50 +47,3 @@ else
 
 json_send($Json);
 ?>
-=======
-<?php
-include 'db_dataset.php';
-include 'json.php';
-
-$sql="insert into logging (ip, url,operation) values (\"".$_SERVER['REMOTE_ADDR']."\",\"http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."\",\"".basename(__FILE__, '.php')."\")";
-mysqli_query($conn, $sql);
-
-$Json = new json();
-
-			
-if (isset($_GET["node_name"]) && isset($_GET["resource_name"]) && isset($_GET["value"]) && isset($_GET["unit"])&& isset($_GET["timestamp"]) && isset($_GET["relative_position"]))
-{	
-
-		$sql = "insert into measurements (node_name,resource_name,value,unit,timestamp,relative_position) values (\""
-		.mysqli_real_escape_string($conn,$_GET["node_name"])."\",\""
-		.mysqli_real_escape_string($conn,$_GET["resource_name"])."\",\""
-		.mysqli_real_escape_string($conn,$_GET["value"])."\",\""
-		.mysqli_real_escape_string($conn,$_GET["unit"])."\",\""
-		.mysqli_real_escape_string($conn,$_GET["timestamp"])."\",\""
-		.mysqli_real_escape_string($conn,$_GET["relative_position"])."\")";
-
-		
-		
-		if (mysqli_query($conn, $sql))
-		{
-			$last_id = mysqli_insert_id($conn);		
-			$Json->addContent(new textJson("Success"));
-			$Json->addContent(new propertyJson("ID",$last_id));
-		}
-		else
-		{
-			$Json->addContent(new textJson("Fail"));
-			if (isset($_GET["debug"]))
-			{
-				echo $sql;
-			}
-		}
-}
-else
-{
-	$Json->addContent(new textJson("Please provide node_name,resource_name,value,unit,timestamp,relative_position in arguments."));
-}
-
-json_send($Json);
-?>
->>>>>>> 27d9b6c62a9e744b3ec8b82881bc1984279cfbee
